372d531900b8a5d79b41d57851e403328fbe14a5
ASrdkn <uyalina098i@gmail.com>
2025-05-08 15:08:17 +0700

    Рефакторинг на MVP: создание MainPresenter

---
+++ app/src/main/java/com/example/lbwatch/main_activity/MainPresenter.kt
@@ -0,0 +1,35 @@
+package com.example.lbwatch.main_activity
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.asLiveData
+import com.example.lbwatch.model.Movie
+import com.example.lbwatch.model.MovieDB
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MainPresenter(private val view: MainView, private val movieDb: MovieDB) {
+
+    fun loadMovies() {
+        movieDb.getDao().getAll().asLiveData().observe(view as LifecycleOwner) { movies: List<Movie> -> // Указываем тип List<Movie>
+            if (movies.isNotEmpty()) {
+                view.showMovies(movies)
+            } else {
+                view.showEmptyState()
+            }
+        }
+    }
+
+
+    fun deleteMovies(movies: List<Movie>) {
+        CoroutineScope(Dispatchers.IO).launch {
+            for (movie in movies) {
+                movieDb.getDao().delete(movie) // Удаление фильма через DAO
+            }
+            launch(Dispatchers.Main) {
+                view.showToast("Фильмы успешно удалены")
+                loadMovies() // Перезагружаем список фильмов после удаления
+            }
+        }
+    }
+}
