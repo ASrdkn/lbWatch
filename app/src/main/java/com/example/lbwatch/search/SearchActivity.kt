0778109e7ce5958e8919e11d65de1a01ff5bf6d0
ASrdkn <uyalina098i@gmail.com>
2025-05-08 16:22:35 +0700

    Рефакторинг на MVP: изменение SearchActivity(добавление View, Presenter)

--- app/src/main/java/com/example/lbwatch/search/SearchActivity.kt
+++ app/src/main/java/com/example/lbwatch/search/SearchActivity.kt
@@ -9,20 +9,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.lbwatch.R
-import com.example.lbwatch.api.ClientAPI
 import com.example.lbwatch.model.Item
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 
-class SearchActivity : AppCompatActivity() {
-    private lateinit var list: List<Item>
+class SearchActivity : AppCompatActivity(), SearchView, SearchAdapterListener {
+
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: SearchAdapter
     private lateinit var noMoviesTextView: TextView
     private lateinit var progressBar: ProgressBar
+    private lateinit var presenter: SearchPresenter
     private var query = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,42 +29,51 @@ class SearchActivity : AppCompatActivity() {
         noMoviesTextView = findViewById(R.id.no_movies_textview)
         recyclerView.layoutManager = LinearLayoutManager(this)
 
-        val i = intent
-        query = i.getStringExtra(SEARCH_QUERY) ?: ""
+        val intent = intent
+        query = intent.getStringExtra(SEARCH_QUERY) ?: ""
 
-        val retrofit: Retrofit = Retrofit.Builder()
-            .baseUrl("https://www.omdbapi.com/")
-            .addConverterFactory(MoshiConverterFactory.create())
-            .build()
+        // Инициализация презентера
+        presenter = SearchPresenter(this)
 
-        val clientApi: ClientAPI = retrofit.create(ClientAPI::class.java)
-
-        CoroutineScope(Dispatchers.IO).launch {
-            val response = clientApi.fetchResponse("c54d9bf7", query)
-            runOnUiThread {
-                list = response.items ?: emptyList()
-                if (list.isEmpty()) {
-                    noMoviesTextView.visibility = View.VISIBLE
-                } else {
-                    noMoviesTextView.visibility = View.INVISIBLE
-                }
-                adapter = SearchAdapter(list, itemListener, this@SearchActivity)
-                recyclerView.adapter = adapter
-                progressBar.visibility = View.INVISIBLE
-            }
-        }
+        presenter.searchMovies(query)
     }
 
-    internal var itemListener: RecyclerItemListener = object : RecyclerItemListener {
-        override fun onItemClick(view: View, position: Int) {
-            val movie = adapter.getItemAtPosition(position)
-            val replyIntent = Intent()
-            replyIntent.putExtra(EXTRA_TITLE, movie.title)
-            replyIntent.putExtra(EXTRA_RELEASE_DATE, movie.getReleaseYearFromDate().toString())
-            replyIntent.putExtra(EXTRA_POSTER_PATH, movie.posterPath)
-            setResult(RESULT_OK, replyIntent)
-            finish()
-        }
+    // Реализуем методы из SearchView
+    override fun showSearchResults(items: List<Item>) {
+        noMoviesTextView.visibility = View.INVISIBLE
+        recyclerView.visibility = View.VISIBLE
+        adapter = SearchAdapter(items, this@SearchActivity, this@SearchActivity)
+        recyclerView.adapter = adapter
+        progressBar.visibility = View.INVISIBLE
+    }
+
+    override fun showError(message: String) {
+        progressBar.visibility = View.INVISIBLE
+        noMoviesTextView.visibility = View.VISIBLE
+        noMoviesTextView.text = message
+    }
+
+    override fun showLoading() {
+        progressBar.visibility = View.VISIBLE
+        recyclerView.visibility = View.INVISIBLE
+        noMoviesTextView.visibility = View.INVISIBLE
+    }
+
+    override fun showEmptyState() {
+        recyclerView.visibility = View.INVISIBLE
+        noMoviesTextView.visibility = View.VISIBLE
+        noMoviesTextView.text = "Нет фильмов для данного поиска"
+    }
+
+    // Реализуем метод из SearchAdapterListener
+    override fun onItemClick(view: View, position: Int) {
+        val movie = adapter.getItemAtPosition(position)
+        val replyIntent = Intent()
+        replyIntent.putExtra(EXTRA_TITLE, movie.title)
+        replyIntent.putExtra(EXTRA_RELEASE_DATE, movie.getReleaseYearFromDate().toString())
+        replyIntent.putExtra(EXTRA_POSTER_PATH, movie.posterPath)
+        setResult(RESULT_OK, replyIntent)
+        finish()
     }
 
     companion object {
@@ -77,9 +81,5 @@ class SearchActivity : AppCompatActivity() {
         const val EXTRA_TITLE = "SearchActivity.TITLE_REPLY"
         const val EXTRA_RELEASE_DATE = "SearchActivity.RELEASE_DATE_REPLY"
         const val EXTRA_POSTER_PATH = "SearchActivity.POSTER_PATH_REPLY"
-    }
-
-    interface RecyclerItemListener {
-        fun onItemClick(v: View, position: Int)
     }
 }
